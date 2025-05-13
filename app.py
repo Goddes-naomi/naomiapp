075a2a510b399cf9afe4072cf0175192caf6ef36
Goddes-naomi <naomioff00@gmail.com>
2025-05-13 21:49:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html")  # Assicurati che index.html sia nella cartella templates/
 
 @app.route("/get-images")
 def get_images():
-    image_folder = "static/images"
+    image_folder = os.path.join("static", "images")
     images = [f for f in os.listdir(image_folder) if f.lower().endswith((".png", ".jpg", ".jpeg", ".gif"))]
     return jsonify(images)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
